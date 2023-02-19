47224e9dc924fcf6a7b9403a266400a6021f1746
abdulwahab-sa <aws18320@gmail.com>
2023-02-19 19:48:08 +0500

    Connected DB

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 	HOST: 'us-cdbr-east-06.cleardb.net',
-	USER: 'be768ab8878f38',
-	PASSWORD: '8a1a37b4',
-	DB: 'heroku_36600a6b89e1b37',
+	USER: 'b47b174ebf998d',
+	PASSWORD: '340326fc',
+	DB: 'heroku_8b885150d94607e',
 	dialect: 'mysql',
 
 	pool: {
@@ -12,5 +12,3 @@ module.exports = {
 		idle: 10000,
 	},
 };
-
-//mysql://be768ab8878f38:8a1a37b4@us-cdbr-east-06.cleardb.net/heroku_36600a6b89e1b37?reconnect=true
