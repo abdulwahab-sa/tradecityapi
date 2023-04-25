17696eb455051555b2b7007db694f93e20ddb339
Abdul Wahab <aws18320@gmail.com>
2023-04-25 21:58:40 +0500

    Added new Route handlers

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,9 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-const getAllProducts = async (req, res) => {
+// GET CONTROLLERS
+
+const getCategories = async (req, res) => {
 	try {
 		const q = 'SELECT * FROM category';
 		db.query(q, (err, data) => {
@@ -15,75 +17,167 @@ const getAllProducts = async (req, res) => {
 	}
 };
 
-const getProduct = async (req, res) => {
+const getSubcategories = async (req, res) => {
 	try {
-		const result = await Product.findOne({ where: { id: `${req.params.id * 1}` } });
-
-		res.json(result);
-		res.status(200);
+		const q = 'SELECT * FROM subcategory';
+		db.query(q, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data);
+		});
 	} catch (err) {
 		res.send(err);
+	}
+};
+
+const getProducts = async (req, res) => {
+	try {
+		const q = 'SELECT * FROM product';
+		db.query(q, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data);
+		});
+	} catch (err) {
+		res.send(err);
+	}
+};
+
+const getInquiries = async (req, res) => {
+	try {
+		const q = 'SELECT * FROM inquiry';
+		db.query(q, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data);
+		});
+	} catch (error) {
+		res.send(error);
+	}
+};
+
+// POST CONTROLLERS
+
+const createSubcategory = async (req, res) => {
+	try {
+		const subcategory_img = req.files['subcategory_img'][0].buffer;
+		const q = 'INSERT INTO subcategory (`subcategory_title`, `subcategory_img`, `category_category_id`) VALUES (?)';
+		const values = [req.body.title, subcategory_img, req.body.category_id];
+		db.query(q, values, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data, 'Subcategory has been created successfully!');
+		});
+	} catch (error) {
+		res.send(error);
 	}
 };
 
 const createProduct = async (req, res) => {
 	try {
-		const subCategoryImg = req.files['subCategoryImg'][0].buffer;
-		const productImg = req.files['productImg'][0].buffer;
-
-		const result = await Product.create({
-			mainCategory: req.body.mainCategory,
-			subCategory: req.body.subCategory,
-			productName: req.body.productName,
-			article: req.body.article,
-			productDescription: req.body.productDescription,
-			subCategoryImg,
-			productImg,
+		const product_img = req.files['product_img'][0].buffer;
+		const q =
+			'INSERT INTO product (`product_title`, `product_img`, `product_description`, `category_category_id`, `subcategory_subcategory_id`) VALUES (?)';
+		const values = [req.body.title, product_img, req.body.product_description, req.body.category_id, req.body.subcategory_id];
+		db.query(q, values, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data, 'Product has been created successfully!');
 		});
-		res.status(201).send(result);
-		console.log(result);
 	} catch (err) {
 		res.send(err);
 	}
 };
+
+const createInquiry = async (req, res) => {
+	try {
+		const q = 'INSERT INTO inquiry (`name`, `email`, `phone`, `req_qty`, `order_detail`) VALUES (?)';
+		const values = [req.body.name, req.body.email, req.body.phone, req.body.req_qty, req.body.order_detail];
+		db.query(q, values, (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data, 'Inquiry has been created successfully!');
+		});
+	} catch (err) {
+		res.send(err);
+	}
+};
+
+// UPDATE CONTROLLERS
 
 const updateProduct = async (req, res) => {
 	try {
-		const subCategoryImg = req.files['subCategoryImg'][0].buffer;
-		const productImg = req.files['productImg'][0].buffer;
+		const product_id = req.params.id;
+		const product_img = req.files['product_img'][0].buffer;
 
-		const result = await Product.findOne({ where: { id: `${req.params.id * 1}` } });
-		result.set({
-			mainCategory: req.body.mainCategory,
-			subCategory: req.body.subCategory,
-			productName: req.body.productName,
-			article: req.body.article,
-			productDescription: req.body.productDescription,
-			subCategoryImg,
-			productImg,
+		const q =
+			'UPDATE product SET `product_title`=?, `product_img`=?, `product_description`=?, `category_category_id`=?, `subcategory_subcategory_id`=? ';
+
+		const values = [req.body.title, product_img, req.body.product_description, req.body.category_id, req.body.subcategory_id];
+
+		db.query(q, [...values, product_id], (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data, 'Product has been updated');
 		});
-
-		await result.save();
-		res.status(201).send(result);
 	} catch (err) {
 		res.send(err);
 	}
 };
+
+const updateSubcategory = async (req, res) => {
+	try {
+		const subcategory_id = req.params.id;
+		const subcategory_img = req.files['subcategory_img'][0].buffer;
+
+		const q = 'UPDATE subcategory SET `subcategory_title`=?, `subcategory_img`=?,  `category_category_id`=?';
+
+		const values = [req.body.title, subcategory_img, req.body.category_id];
+
+		db.query(q, [...values, subcategory_id], (err, data) => {
+			if (err) return res.json(err);
+			return res.json(data, 'Subcategory has been updated');
+		});
+	} catch (err) {
+		res.send(err);
+	}
+};
+
+// DELETE CONTROLLERS
 
 const deleteProduct = async (req, res) => {
 	try {
-		const result = await Product.destroy({
-			where: {
-				id: `${req.params.id * 1}`,
-			},
-		});
-		res.status(204).json({
-			status: 'success',
-			data: null,
+		const product_id = req.params.id;
+
+		const q = 'DELETE FROM product WHERE product_id = ?';
+
+		db.query(q, [product_id], (err, data) => {
+			if (err) return res.json(err);
+			return res.json('Product has been deleted');
 		});
 	} catch (err) {
 		res.send(err);
 	}
 };
 
-module.exports = { getAllProducts, getProduct, createProduct, deleteProduct, updateProduct };
+const deleteInquiry = async (req, res) => {
+	try {
+		const inquiry_id = req.params.id;
+
+		const q = 'DELETE FROM inquiry WHERE inquiry_id = ?';
+
+		db.query(q, [inquiry_id], (err, data) => {
+			if (err) return res.json(err);
+			return res.json('Inquiry has been deleted');
+		});
+	} catch (err) {
+		res.send(err);
+	}
+};
+
+module.exports = {
+	getCategories,
+	getSubcategories,
+	getProducts,
+	getInquiries,
+	createSubcategory,
+	createProduct,
+	createInquiry,
+	updateProduct,
+	updateSubcategory,
+	deleteProduct,
+	deleteInquiry,
+};
--- routes/route.js
+++ routes/route.js
@@ -1,29 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/controller.js');
+const {
+	getCategories,
+	getSubcategories,
+	getProducts,
+	getInquiries,
+	createSubcategory,
+	createProduct,
+	createInquiry,
+	updateProduct,
+	deleteProduct,
+	deleteInquiry,
+	updateSubcategory,
+} = require('../controller/controller.js');
 const upload = require('./../middleware/multerImage');
 const { loginController, refreshController } = require('./../auth/index');
+
+// Route for pre-defined main categories
+router.route('/categories').get(getCategories);
+
+//Routes for CRUD of Subcategories
 router
-	.route('/categories')
-	.get(getAllProducts)
-	.post(
-		upload.fields([
-			{ name: 'subCategoryImg', maxCount: 1 },
-			{ name: 'productImg', maxCount: 1 },
-		]),
-		createProduct
-	);
+	.route('/subcategories')
+	.get(getSubcategories)
+	.post(upload.fields([{ name: 'subcategory_img', maxCount: 1 }]), createSubcategory);
+
+router.route('/subcategories/:id').put(upload.fields([{ name: 'subcategory_img', maxCount: 1 }]), updateSubcategory);
+
+// Routes for CRUD of Products
+
 router
-	.route('/:id')
-	.get(getProduct)
-	.put(
-		upload.fields([
-			{ name: 'subCategoryImg', maxCount: 1 },
-			{ name: 'productImg', maxCount: 1 },
-		]),
-		updateProduct
-	)
+	.route('/products')
+	.get(getProducts)
+	.post(upload.fields([{ name: 'product_img', maxCount: 1 }]), createProduct);
+
+router
+	.route('/products/:id')
+	.put(upload.fields([{ name: 'product_img', maxCount: 1 }]), updateProduct)
 	.delete(deleteProduct);
+
+// Routes for Inquiry
+
+router.route('/inquiry').get(getInquiries).post(createInquiry);
+router.route('/inquiry/:id').delete(deleteInquiry);
+
+// Route for Admin Panel Login
 
 router.route('/login').post(loginController);
 router.route('/refresh').post(refreshController);
