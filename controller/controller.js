f0bc2a7b1500c9e923481021e69edc0c8b10ae4f
Abdul Wahab <aws18320@gmail.com>
2023-04-30 15:18:09 +0500

    Refactored Code

--- controller/controller.js
+++ controller/controller.js
@@ -8,48 +8,40 @@ const upload = multer({ storage });
 const getCategories = async (req, res) => {
 	try {
 		const q = 'SELECT * FROM category';
-		db.query(q, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data);
-		});
+		const result = await db.query(q);
+		result && res.status(200).json({ result });
 	} catch (err) {
-		res.send(err);
+		res.status(500).json({ err });
 	}
 };
 
 const getSubcategories = async (req, res) => {
 	try {
 		const q = 'SELECT * FROM subcategory';
-		db.query(q, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data);
-		});
+		const result = await db.query(q);
+		result && res.status(200).json({ result });
 	} catch (err) {
-		res.send(err);
+		res.status(500).json({ err });
 	}
 };
 
 const getProducts = async (req, res) => {
 	try {
 		const q = 'SELECT * FROM product';
-		db.query(q, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data);
-		});
+		const result = await db.query(q);
+		result && res.status(200).json({ result });
 	} catch (err) {
-		res.send(err);
+		res.status(500).json({ err });
 	}
 };
 
 const getInquiries = async (req, res) => {
 	try {
 		const q = 'SELECT * FROM inquiry';
-		db.query(q, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data);
-		});
+		const result = await db.query(q);
+		result && res.status(200).json({ result });
 	} catch (error) {
-		res.send(error);
+		res.status(500).json({ err });
 	}
 };
 
@@ -75,10 +67,10 @@ const createSubcategory = async (req, res) => {
 		// Insert subcategory into database
 		const q = 'INSERT INTO subcategory (`subcategory_title`, `subcategory_img`, `category_category_id`) VALUES (?, ?, ?);';
 		const values = [subcategory_title, subcategory_img, category_category_id];
-		const { result } = await db.query(q, values);
+		const result = await db.query(q, values);
 
 		// Return success response
-		return res.status(200).json({ message: 'Subcategory has been created successfully!', data: result });
+		return res.status(201).json({ message: 'Subcategory has been created successfully!', data: result });
 	} catch (error) {
 		// Log error and return error response
 		console.error(error);
@@ -86,47 +78,80 @@ const createSubcategory = async (req, res) => {
 	}
 };
 
-/*
-
-const createSubcategory = async (req, res) => {
-	try {
-		const subcategory_img = req.files['subcategory_img'][0].buffer;
-		const q = 'INSERT INTO subcategory (`subcategory_title`, `subcategory_img`, `category_category_id`) VALUES (?);';
-		const values = [req.body.subcategory_title, subcategory_img, req.body.category_category_id];
-		db.query(q, values, (err, data) => {
-			if (err) return res.json(err);
-			return res.json({ message: 'Subcategory has been created successfully!', data });
-		});
-	} catch (error) {
-		res.send(error);
-	}
-};
-*/
 const createProduct = async (req, res) => {
 	try {
-		const product_img = req.files['product_img'][0].buffer;
+		// Validate request body parameters
+		const { product_title, article, subcategory_subcategory_id, category_category_id } = req.body;
+		if (!product_title) {
+			throw new Error('Product title is required');
+		}
+		if (!article) {
+			throw new Error('Article is required');
+		}
+
+		if (!subcategory_subcategory_id) {
+			throw new Error('Subcategory ID is required');
+		}
+
+		if (!category_category_id) {
+			throw new Error('Category ID is required');
+		}
+
+		// Get product image from request
+		const product_img = req.files?.['product_img']?.[0]?.buffer;
+		if (!product_img) {
+			throw new Error('No product image found in request');
+		}
+
+		// Insert product into database
 		const q =
-			'INSERT INTO product (`product_title`, `product_img`, `product_description`, `category_category_id`, `subcategory_subcategory_id`) VALUES (?)';
-		const values = [req.body.title, product_img, req.body.product_description, req.body.category_id, req.body.subcategory_id];
-		db.query(q, values, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data, 'Product has been created successfully!');
-		});
-	} catch (err) {
-		res.send(err);
+			'INSERT INTO product (`product_title`, `product_img`, `product_description`, `category_category_id`, `subcategory_subcategory_id`) VALUES (?, ?, ?, ?, ?);';
+		const values = [product_title, product_img, article, category_category_id, subcategory_subcategory_id];
+		const result = await db.query(q, values);
+
+		// Return success response
+		return res.status(201).json({ message: 'Product has been created successfully!', data: result });
+	} catch (error) {
+		// Log error and return error response
+		console.error(error);
+		return res.status(500).json({ message: 'Error creating product', error: error.message });
 	}
 };
 
 const createInquiry = async (req, res) => {
 	try {
-		const q = 'INSERT INTO inquiry (`name`, `email`, `phone`, `req_qty`, `order_detail`) VALUES (?)';
-		const values = [req.body.name, req.body.email, req.body.phone, req.body.req_qty, req.body.order_detail];
-		db.query(q, values, (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data, 'Inquiry has been created successfully!');
-		});
-	} catch (err) {
-		res.send(err);
+		// Validate request body parameters
+		const { name, email, phone, req_qty, order_detail } = req.body;
+		if (!name) {
+			throw new Error('Name is required');
+		}
+		if (!email) {
+			throw new Error('Email is required');
+		}
+
+		if (!phone) {
+			throw new Error('Phone is required');
+		}
+
+		if (!req_qty) {
+			throw new Error('Quantity is required');
+		}
+
+		if (!order_detail) {
+			throw new Error('Order Detail is required');
+		}
+
+		// Insert inquiry into database
+		const q = 'INSERT INTO inquiry (`name`, `email`, `phone`, `req_qty`, `order_detail`) VALUES (?, ?, ?, ?, ?);';
+		const values = [name, email, article, req_qty, order_detail];
+		const result = await db.query(q, values);
+
+		// Return success response
+		return res.status(201).json({ message: 'Inquiry has been created successfully!', data: result });
+	} catch (error) {
+		// Log error and return error response
+		console.error(error);
+		return res.status(500).json({ message: 'Error creating inquiry', error: error.message });
 	}
 };
 
@@ -134,38 +159,52 @@ const createInquiry = async (req, res) => {
 
 const updateProduct = async (req, res) => {
 	try {
+		// Validate request body parameters
 		const product_id = req.params.id;
-		const product_img = req.files['product_img'][0].buffer;
+		const { product_title, article, product_description, subcategory_subcategory_id, category_category_id } = req.body;
+
+		// Get product image from request
+		const product_img = req.files?.['product_img']?.[0]?.buffer;
+		if (!product_img) {
+			throw new Error('No product image found in request');
+		}
+
+		// Insert Product into database
 
 		const q =
 			'UPDATE product SET `product_title`=?, `product_img`=?, `product_description`=?, `category_category_id`=?, `subcategory_subcategory_id`=? ';
 
-		const values = [req.body.title, product_img, req.body.product_description, req.body.category_id, req.body.subcategory_id];
+		const values = [product_title, article, product_img, product_description, subcategory_subcategory_id, category_category_id];
 
-		db.query(q, [...values, product_id], (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data, 'Product has been updated');
-		});
+		const result = await db.query(q, [...values, product_id]);
+
+		// Return success response
+		res.status(200).json({ message: 'Product has been updated successfully!', data: result });
 	} catch (err) {
-		res.send(err);
+		return res.status(500).json({ message: 'Error updating product', error: err.message });
 	}
 };
 
 const updateSubcategory = async (req, res) => {
 	try {
+		// Validate request body parameters
 		const subcategory_id = req.params.id;
+		const [subcategory_title, category_category_id] = req.body;
+
+		// Get Subcategory Image from request
 		const subcategory_img = req.files['subcategory_img'][0].buffer;
+
+		// Insert Subcategory into database
 
 		const q = 'UPDATE subcategory SET `subcategory_title`=?, `subcategory_img`=?,  `category_category_id`=?';
 
-		const values = [req.body.title, subcategory_img, req.body.category_id];
+		const values = [subcategory_title, subcategory_img, category_category_id];
+		const result = await db.query(q, [...values, subcategory_id]);
 
-		db.query(q, [...values, subcategory_id], (err, data) => {
-			if (err) return res.json(err);
-			return res.json(data, 'Subcategory has been updated');
-		});
+		// Return success response
+		return res.status(200).json({ message: 'Subcategory has been updated successfully!', data: result });
 	} catch (err) {
-		res.send(err);
+		return res.status(500).json({ message: 'Error updating subcategory', error: err.message });
 	}
 };
 
@@ -174,15 +213,10 @@ const updateSubcategory = async (req, res) => {
 const deleteProduct = async (req, res) => {
 	try {
 		const product_id = req.params.id;
-
-		const q = 'DELETE FROM product WHERE product_id = ?';
-
-		db.query(q, [product_id], (err, data) => {
-			if (err) return res.json(err);
-			return res.json('Product has been deleted');
-		});
+		const result = await db.query(q, [product_id]);
+		result && res.status(200).json({ message: 'Product deleted successfully!' });
 	} catch (err) {
-		res.send(err);
+		return res.status(500).json({ message: 'Error deleting product', error: err.message });
 	}
 };
 
@@ -192,12 +226,10 @@ const deleteInquiry = async (req, res) => {
 
 		const q = 'DELETE FROM inquiry WHERE inquiry_id = ?';
 
-		db.query(q, [inquiry_id], (err, data) => {
-			if (err) return res.json(err);
-			return res.json('Inquiry has been deleted');
-		});
+		const result = await db.query(q, [inquiry_id]);
+		result && res.status(200).json({ message: 'Inquiry deleted successfully!' });
 	} catch (err) {
-		res.send(err);
+		return res.status(500).json({ message: 'Error deleting inquiry', error: err.message });
 	}
 };
 
