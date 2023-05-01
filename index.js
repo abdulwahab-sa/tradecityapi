abce3550cfa4b725379f7418fb557d7eb150c248
Abdul Wahab <aws18320@gmail.com>
2023-05-01 18:23:40 +0500

    Refactored Code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-	origin: '*',
+	origin: 'http://localhost:5173',
 	credentials: true,
 };
 app.use(cors(corsOptions));
