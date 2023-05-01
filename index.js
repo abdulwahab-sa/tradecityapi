a003898fdc34ca615daf917891f30dc41476994b
Abdul Wahab <aws18320@gmail.com>
2023-05-01 18:17:44 +0500

    Refactored code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(cors(), function (req, res, next) {
+app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
