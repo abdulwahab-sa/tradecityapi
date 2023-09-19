eb86ef4420444ff30b499af0331c77469139b1be
Abdul Wahab <aws18320@gmail.com>
2023-09-19 17:59:51 +0500

    Refactored MYSQL

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
 	host: process.env.MYSQLHOST,
 	user: process.env.MYSQLUSERNAME,
 	password: process.env.MYSQLPASSWORD,
 	database: process.env.MYSQLDATABASE,
 	port: process.env.MYSQLPORT,
+	connectionLimit: 10,
 });
 
 module.exports = db;
