42a5e6cf077ed81e398b438aaddb42fea1f05339
Abdul Wahab <aws18320@gmail.com>
2024-02-02 12:58:37 +0500

    Refactored

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-	origin: 'https://tradecity.netlify.app/',
+	origin: '*',
 	credentials: true,
 };
 app.use(cors(corsOptions));
 
+/*
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'https://tradecity.netlify.app/');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
 	next();
 });
-
+*/
 app.use('/api', router);
 
 app.listen(process.env.PORT || 5000, () => {
